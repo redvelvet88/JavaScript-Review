0be629fb6123b6411a81792244dcec9e450ccb94
redvelvet88 <abbeyrollins@hotmail.com>
2016-05-22 19:24:41 -0600

    save1

--- arrayReview.js
+++ arrayReview.js
@@ -3,24 +3,54 @@ var threeItems = [1,2,3];
 //alert the result of your function
 
   //code here
+var last = function() {
+  console.log(threeItems[(threeItems.length -1)]);
+}
 
+// last(threeItems);
+
+
+
+var threeItems = [1,2,3];
+
+for (var i = 0; i < threeItems.length; i++) {
+	console.log(threeItems[i]);
+};
+
+threeItems.forEach(function (item) {
+	console.log(item);
+});
+
+var newArray = threeItems.map(function (item) {
+	return "hi";
+});
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Loop through evenArray removing all values that aren't even 
+//Loop through evenArray removing all values that aren't even
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
 
+	for (var i = 0; i < evenArray.length; i++) {
+		if ((evenArray[i] % 2) === 1) {
+			evenArray.splice(i, 1);
+			i--;
 
+		}
+	}
+
+console.log(evenArray);
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'.
+// Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array.
+// If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
@@ -28,19 +58,33 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
 
+  var random = function(arr) {
+	    var randomNum = getRandomArbitrary();
+	    console.log(randomNum);
+	    if(arr.indexOf(randomNum) !== -1) {
+	        alert(true);
+	    } else {
+	        alert(false);
+	    }
+	};
 
+  // getRandomArbitrary(randomArray);
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this,
+//first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+
 var first = [1,2,3,4,5];
-var second;
 
   //code here
+var second = first.slice(0, 5);
+second.push(6, 7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
+
 
 
 
@@ -49,21 +93,34 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
-var sentence = "Dev Mountain is the best"
+var sentence = "Dev Mountain is the best";
 
   //code here
-
+var longest = function(str) {
+	var string = str.split(" ");
+	var longestWord = ("");
+	for (var i = 0; i < string.length; i++) {
+		if(longestWord.length < string[i].length) {
+			longestWord = string[i];
+		}
+	}
+	return longestWord;
+};
+longest(sentence);
+//longest(sentence);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
+//write a function called capitalize that takes in the myPoem variable and capitalizes every word
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
   //code here
+var capitalize = function(str) {
 
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
--- objectReview.js
+++ objectReview.js
@@ -1,19 +1,31 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values
+// to those keys be your favorite thing in that category.
 
   //Your code here
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+var favoriteThings = {
+  band: "Judah & the Lion",
+  food: "French Fries",
+  person: "Mom",
+  book: "Book of Mormon",
+  movie: "A Knight's Tale",
+  holiday: "4th of July"
+};
+//After you've made your object, add another key named 'car' with the value being your favorite car and then another key
+//named 'brand' with the value being your favorite brand.
 
   //Your code here
-
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+favoriteThings.car = "Elantra";
+favoriteThings.brand = "Hyundai";
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Your code here
-
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 //Now, alert your favorite person, then alert your favorite book.
 
   //Your code here
-
+  alert(favoriteThings.person);
+  alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -21,7 +33,7 @@
 
 
 var user = {
-    name: 'Tyler McGinnis', 
+    name: 'Tyler McGinnis',
     email: null,
     pwHash: 'U+Ldlngx2BYQk',
     birthday: undefined,
@@ -29,44 +41,72 @@ var user = {
     age: 0
 }
 
-//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
+//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object.
+// hint: 'delete'.
 
   //Your code here
-
+for(key in user) {
+  if(!user[key]) {
+    delete(user[key])
+  };
+};
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Your code here
-
+user.name = "Abbey Rollins";
+user.username = "arollins";
+user.pwHash = "password";
 //Now console.log your object and make sure it looks right.
 
   //Your code here
-
-
-
-/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
-
-//create an empty object called methodCollection. 
-
-  //Your code here
-
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
-
-  //Your code here
-
-//Now call your alertHello and logHello methods. 
-
-  //Your code here
-
+// console.log(user);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
+//create an empty object called methodCollection.
+
+  //Your code here
+var methodCollection = {};
+//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another
+//method called logHello which logs 'hello' to the console.
+
+  //Your code here
+methodCollection.alertHello = function() {
+  alert("hello");
+}
+
+methodCollection.logHello = function() {
+  console.log("hello");
+}
+//Now call your alertHello and logHello methods.
+
+  //Your code here
+methodCollection.alertHello();
+methodCollection.logHello();
+
+
+/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+
+
+
+//write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string,
+//and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
 
+var voweler = function (str) {
+  var strArr = str.split('');
+  var obj = {};
+  strArr.forEach(function(ch) {
+    if ('aeiou'.indexOf(ch) !== -1) {
+      obj[ch] = (obj[ch] || 0 ) + 1;
+    }
+  });
+  return obj;
+}
+console.log(voweler("This is a test"));
 
-
+// Your loop will assign the property value for the key to 0 every time the character is a vowel, before incrementing.
+// You can use indexOf. It will return -1 if it cannot find the argument in a string.
